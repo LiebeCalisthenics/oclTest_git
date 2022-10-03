7937ea59db63b2f1c6eb3ed9758134dc36c43496
oasis16z <oasis16z@naver.com>
2022-10-03 11:41:31 +0900

    [team04]
    Author : 김선규
    Detail : oclTest 스프링 프로젝트 생성

---
+++ oclTest/src/main/java/ksmart/oclTest/OclTestApplication.java
@@ -0,0 +1,13 @@
+package ksmart.oclTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OclTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OclTestApplication.class, args);
+	}
+
+}
---
+++ oclTest/src/test/java/ksmart/oclTest/OclTestApplicationTests.java
@@ -0,0 +1,13 @@
+package ksmart.oclTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OclTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
